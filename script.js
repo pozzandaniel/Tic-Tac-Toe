5f7a51950f84202614a3d79b10243d66308c3008
Daniel Pozzan <pozzan.dnl@gmail.com>
2022-04-24 17:15:33 +0200

    shape added

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 let game = [];
-let currentShape = 'cross'
+let currentShape = 'cross';
 
 function fillGame(id) {
     game[id] = currentShape;
     console.log(game);
-
+    draw(id);
     if(currentShape == 'cross'){
         currentShape = 'circle';
     } else {
         currentShape = 'cross';
     }
+}
 
 
+function draw(id){
+    for (let i = 0; i < game.length; i++) {
+        if(currentShape == 'cross'){
+            document.getElementById('cross-' + id).classList.remove('d-none');
+            
+        } 
+        
+        if(currentShape == 'circle'){
+            document.getElementById('circle-' + id).classList.remove('d-none');
+        }
+    }
 }
