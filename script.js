b10e2e80f8d33b1146cbb2662f46f612c627ea1a
Daniel Pozzan <pozzan.dnl@gmail.com>
2022-04-24 17:47:10 +0200

    winner determinate

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function fillGame(id) {
     game[id] = currentShape;
     console.log(game);
     draw(id);
+    controllWinner();
     if(currentShape == 'cross'){
         currentShape = 'circle';
     } else {
@@ -23,5 +24,36 @@ function draw(id){
         if(currentShape == 'circle'){
             document.getElementById('circle-' + id).classList.remove('d-none');
         }
+    }
+}
+
+function controllWinner(){
+    let winner;
+    if(game[0] == game[1] && game[1] == game[2] && game[0]){
+        winner = game[0];
+    }
+    if(game[3] == game[4] && game[4] == game[5] && game[3]){
+        winner = game[3];
+    }
+    if(game[6] == game[7] && game[7] == game[8] && game[6]){
+        winner = game[6];
+    }
+    if(game[0] == game[3] && game[3] == game[6] && game[0]){
+        winner = game[0];
+    }
+    if(game[1] == game[4] && game[4] == game[7] && game[1]){
+        winner = game[1];
+    }
+    if(game[2] == game[5] && game[5] == game[8] && game[2]){
+        winner = game[2];
+    }
+    if(game[0] == game[4] && game[4] == game[8] && game[0]){
+        winner = game[0];
+    }
+    if(game[2] == game[4] && game[4] == game[6] && game[2]){
+        winner = game[2];
+    }
+    if(winner){
+        console.log(winner + ' hat gewonnen!');
     }
 }
